a9b96d8bf26d18f4add4ca1b31962ec56595d40b
Forrest <69464353+linsnotes@users.noreply.github.com>
2024-11-30 14:19:08 +0800

    clean unused files

--- assets/js/auto-update.js
+++
@@ -1,5 +0,0 @@
-const toast = document.getElementById('notification');
-toast.addEventListener('shown.bs.toast', () => {
-  const button = toast.querySelector('.toast-body>button');
-  button?.click();
-});
